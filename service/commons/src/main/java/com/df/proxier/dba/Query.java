3be6b3359f87c77d94b612257a78bff619dd25c2
陈宏葆 <chenhongbao@outlook.com>
2020-12-18 11:59:57 +0800

    Add primary key when creating table.

--- service/commons/src/main/java/com/df/proxier/dba/Query.java
+++ service/commons/src/main/java/com/df/proxier/dba/Query.java
@@ -36,6 +36,7 @@ import java.util.Map;
  */
 class Query implements IQuery {
 
+    private final String PRIMARY_KEY = "PRIMARY KEY";
     private final Connection conn;
     private final Map<String, MetaTable<?>> meta;
 
@@ -89,6 +90,38 @@ class Query implements IQuery {
         }
     }
 
+    private String buildFieldPair(MetaField f) {
+        return f.getName() + " " + DbaUtils.convertSqlType(f.getType());
+    }
+
+    private <T> String buildFieldPairs(MetaTable<T> meta) throws DbaException {
+        if (meta.fields().isEmpty()) {
+            throw new DbaException("No field.");
+        }
+        else {
+            String sql = "";
+            int i = 0;
+            while (i < meta.fields().size() - 1) {
+                var f = meta.fields().get(i);
+                sql += buildFieldWithKey(f, meta) + ",";
+                ++i;
+            }
+            sql += buildFieldWithKey(meta.fields().get(i), meta);
+            if (!sql.contains(PRIMARY_KEY)) {
+                throw new DbaException("Table '" + meta.getName() + "' has no primary key.");
+            }
+            return sql;
+        }
+    }
+
+    private <T> String buildFieldWithKey(MetaField f, MetaTable<T> meta) {
+        String sql = buildFieldPair(f);
+        if (isPrimaryKey(f, meta)) {
+            sql += " " + PRIMARY_KEY;
+        }
+        return sql;
+    }
+
     private <T> Collection<T> convert(MetaTable<T> meta,
                                       ResultSet rs,
                                       IDefaultFactory<T> factory) throws ReflectiveOperationException,
@@ -98,6 +131,110 @@ class Query implements IQuery {
             c.add(rowT(meta, rs, factory));
         }
         return c;
+    }
+
+    private <T> void createTable(MetaTable<T> meta) throws DbaException, SQLException {
+        String sql = "CREATE TABLE " + meta.getName() + "(";
+        sql += buildFieldPairs(meta);
+        sql += ")";
+        execute(sql);
+    }
+
+    private <T> void ensureTable(MetaTable<T> meta) throws SQLException,
+                                                           DbaException {
+        var dbm = conn.getMetaData();
+        if (!hasTableName(meta, dbm)) {
+            createTable(meta);
+        }
+        else {
+            verifyTableColumns(meta, dbm);
+        }
+    }
+
+    private boolean equalsType(int columnType, int semanticType) throws DbaException {
+        switch (semanticType) {
+            case Types.CHAR:
+            case Types.BIGINT:
+            case Types.INTEGER:
+            case Types.DECIMAL:
+                return columnType == semanticType;
+            case Types.DATE:
+            case Types.TIMESTAMP_WITH_TIMEZONE:
+                return columnType == Types.CHAR;
+        }
+        throw new DbaException("Invalid field type in table metadata.");
+    }
+
+    private int execute(String sql) throws SQLException {
+        try (Statement stat = conn.createStatement()) {
+            stat.execute(sql);
+            return stat.getUpdateCount();
+        }
+    }
+
+    private <T> Collection<T> executeSelect(MetaTable<T> meta,
+                                            String sql,
+                                            IDefaultFactory<T> factory) throws SQLException,
+                                                                               ReflectiveOperationException {
+        ResultSet rs;
+        try (Statement stat = conn.createStatement()) {
+            rs = stat.executeQuery(sql);
+            return convert(meta, rs, factory);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> MetaTable<T> findMeta(Class<T> clazz) {
+        return (MetaTable<T>) meta.computeIfAbsent(clazz.getCanonicalName(), k -> new MetaTable<T>(clazz));
+    }
+
+    private <T> String getInsertSql(MetaTable<T> meta,
+                                    Object object) throws SQLException,
+                                                          DbaException {
+        ensureTable(meta);
+        return ""; // TODO getInsertSql
+    }
+
+    private <T> String getRemoveSql(MetaTable<T> meta,
+                                    ICondition<?> condition) throws SQLException,
+                                                                    DbaException {
+        ensureTable(meta);
+        return ""; // TODO getRemoveSql
+    }
+
+    private <T> String getSelectSql(MetaTable<T> meta,
+                                    ICondition<?> condition) throws SQLException,
+                                                                    DbaException {
+        ensureTable(meta);
+        return ""; // TODO getSelectSql
+    }
+
+    private Map<String, Integer> getTableColumns(String name, DatabaseMetaData dbMeta) throws SQLException {
+        var t = new HashMap<String, Integer>(128);
+        var cs = dbMeta.getColumns("", "", name, "%");
+        while (cs.next()) {
+            t.put(cs.getString("COLUMN_NAME"), cs.getInt("DATA_TYPE"));
+        }
+        return t;
+    }
+
+    private <T> String getUpdateSql(MetaTable<T> meta,
+                                    Object object,
+                                    ICondition<?> condition) throws SQLException,
+                                                                    DbaException {
+        ensureTable(meta);
+        return ""; // TODO getUpdateSql
+    }
+
+    private <T> boolean hasTableName(MetaTable<T> meta, DatabaseMetaData dbMeta) throws SQLException {
+        try (var rs = dbMeta.getTables("", "", meta.getName(), null)) {
+            return rs.next();
+        }
+    }
+
+    private <T> boolean isPrimaryKey(MetaField f, MetaTable<T> table) {
+        var pkn = table.getName().toLowerCase() + "id";
+        return f.getField().getName().compareToIgnoreCase(pkn) == 0;
     }
 
     private <T> T rowT(MetaTable<T> meta, ResultSet rs, IDefaultFactory<T> factory) throws SQLException {
@@ -140,46 +277,6 @@ class Query implements IQuery {
         }
     }
 
-    private int execute(String sql) throws SQLException {
-        try (Statement stat = conn.createStatement()) {
-            stat.execute(sql);
-            return stat.getUpdateCount();
-        }
-    }
-
-    private <T> Collection<T> executeSelect(MetaTable<T> meta,
-                                            String sql,
-                                            IDefaultFactory<T> factory) throws SQLException,
-                                                                               ReflectiveOperationException {
-        ResultSet rs;
-        try (Statement stat = conn.createStatement()) {
-            rs = stat.executeQuery(sql);
-            return convert(meta, rs, factory);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T> MetaTable<T> findMeta(Class<T> clazz) {
-        return (MetaTable<T>) meta.computeIfAbsent(clazz.getCanonicalName(), k -> new MetaTable<T>(clazz));
-    }
-
-    private <T> void ensureTable(MetaTable<T> meta) throws SQLException,
-                                                           DbaException {
-        var dbm = conn.getMetaData();
-        if (!hasTableName(meta, dbm)) {
-            createTable(meta);
-        }
-        else {
-            verifyTableColumns(meta, dbm);
-        }
-    }
-
-    private <T> boolean hasTableName(MetaTable<T> meta, DatabaseMetaData dbMeta) throws SQLException {
-        try (var rs = dbMeta.getTables("", "", meta.getName(), null)) {
-            return rs.next();
-        }
-    }
-
     private <T> void verifyTableColumns(MetaTable<T> meta, DatabaseMetaData dbMeta) throws SQLException,
                                                                                            DbaException {
         var m = getTableColumns(meta.getName(), dbMeta);
@@ -192,86 +289,6 @@ class Query implements IQuery {
                 throw new DbaException("Field " + f.getName() + " has wrong type.");
             }
         }
-    }
-
-    private boolean equalsType(int columnType, int semanticType) throws DbaException {
-        switch (semanticType) {
-            case Types.CHAR:
-            case Types.BIGINT:
-            case Types.INTEGER:
-            case Types.DECIMAL:
-                return columnType == semanticType;
-            case Types.DATE:
-            case Types.TIMESTAMP_WITH_TIMEZONE:
-                return columnType == Types.CHAR;
-        }
-        throw new DbaException("Invalid field type in table metadata.");
-    }
-
-    private Map<String, Integer> getTableColumns(String name, DatabaseMetaData dbMeta) throws SQLException {
-        var t = new HashMap<String, Integer>(128);
-        var cs = dbMeta.getColumns("", "", name, "%");
-        while (cs.next()) {
-            t.put(cs.getString("COLUMN_NAME"), cs.getInt("DATA_TYPE"));
-        }
-        return t;
-    }
-
-    private <T> void createTable(MetaTable<T> meta) throws DbaException, SQLException {
-        String sql = "CREATE TABLE " + meta.getName() + "(";
-        sql += buildFieldPairs(meta);
-        sql += ")";
-        execute(sql);
-    }
-
-    private <T> String buildFieldPairs(MetaTable<T> meta) throws DbaException {
-        if (meta.fields().isEmpty()) {
-            throw new DbaException("No field.");
-        }
-        else {
-            String sql = "";
-            int i = 0;
-            while (i < meta.fields().size() - 1) {
-                var f = meta.fields().get(i);
-                sql += buildFieldPair(f) + ",";
-                ++i;
-            }
-            sql += buildFieldPair(meta.fields().get(i));
-            return sql;
-        }
-    }
-
-    private String buildFieldPair(MetaField f) {
-        return f.getName() + " " + DbaUtils.convertSqlType(f.getType());
-    }
-
-    private <T> String getInsertSql(MetaTable<T> meta,
-                                    Object object) throws SQLException,
-                                                          DbaException {
-        ensureTable(meta);
-        return ""; // TODO getInsertSql
-    }
-
-    private <T> String getRemoveSql(MetaTable<T> meta,
-                                    ICondition<?> condition) throws SQLException,
-                                                                    DbaException {
-        ensureTable(meta);
-        return ""; // TODO getRemoveSql
-    }
-
-    private <T> String getSelectSql(MetaTable<T> meta,
-                                    ICondition<?> condition) throws SQLException,
-                                                                    DbaException {
-        ensureTable(meta);
-        return ""; // TODO getSelectSql
-    }
-
-    private <T> String getUpdateSql(MetaTable<T> meta,
-                                    Object object,
-                                    ICondition<?> condition) throws SQLException,
-                                                                    DbaException {
-        ensureTable(meta);
-        return ""; // TODO getUpdateSql
     }
 
 }
